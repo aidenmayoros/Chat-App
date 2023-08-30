fe02ca2cfdca923b155cf2507d70c7a898b3e94a
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-08-29 20:53:03 -0700

    5.2 submission

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,70 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
 	const { name } = route.params;
 	const { color } = route.params;
 	const dynamicStyledBackground = { ...styles.container, backgroundColor: color };
 
+	const [messages, setMessages] = useState([]);
+
 	useEffect(() => {
 		navigation.setOptions({ title: name });
+		setMessages([
+			{
+				_id: 1,
+				text: 'Hello developer',
+				createdAt: new Date(),
+				user: {
+					_id: 2,
+					name: 'React Native',
+					avatar: 'https://placeimg.com/140/140/any',
+				},
+			},
+			{
+				_id: 2,
+				text: 'test -- This is a system message',
+				createdAt: new Date(),
+				system: true,
+			},
+		]);
 	}, []);
+
+	const onSend = (newMessages) => {
+		setMessages((previousMessages) => GiftedChat.append(previousMessages, newMessages));
+	};
+
+	const renderBubble = (props) => {
+		return (
+			<Bubble
+				{...props}
+				wrapperStyle={{
+					right: {
+						backgroundColor: '#000',
+					},
+					left: {
+						backgroundColor: '#FFF',
+					},
+				}}
+			/>
+		);
+	};
 
 	return (
 		<View style={dynamicStyledBackground}>
-			<Text style={{ color: 'white' }}>Chat screen</Text>
+			<GiftedChat
+				textInputStyle={styles.chatInput}
+				messages={messages}
+				renderBubble={renderBubble}
+				onSend={(messages) => onSend(messages)}
+				user={{
+					_id: 1,
+				}}
+			/>
+			{Platform.OS === 'android' || Platform.OS === 'ios' ? (
+				<KeyboardAvoidingView behavior='height' />
+			) : null}
 		</View>
 	);
 };
@@ -20,8 +72,9 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
+	},
+	chatInput: {
+		padding: 10,
 	},
 });
 
